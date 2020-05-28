c5b0f2e72cddb75f51237b3de809be1815c022ba
Lorena Toro <lorenatorog11@gmail.com>
2020-05-28 09:13:44 -0500

    Updates

---
+++ precache-manifest.7d9073af374521dee29b4acebd155ed5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2324b5a978f8aa1ac5660fe4888cab05",
+    "url": "/plantaap/index.html"
+  },
+  {
+    "revision": "13551d682f425aac2a91",
+    "url": "/plantaap/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "42189d0e4406897839a5",
+    "url": "/plantaap/static/css/main.0a54a213.chunk.css"
+  },
+  {
+    "revision": "13551d682f425aac2a91",
+    "url": "/plantaap/static/js/2.e150fbdd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/plantaap/static/js/2.e150fbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42189d0e4406897839a5",
+    "url": "/plantaap/static/js/main.c11ece76.chunk.js"
+  },
+  {
+    "revision": "496bf7a6b1d56f31db05",
+    "url": "/plantaap/static/js/runtime-main.2e63ff50.js"
+  },
+  {
+    "revision": "ba631b638722bb02c7a4ee1998ba6224",
+    "url": "/plantaap/static/media/banner_img.ba631b63.jpg"
+  },
+  {
+    "revision": "0d3d721254ae1141a1533f413fcd768c",
+    "url": "/plantaap/static/media/logo.0d3d7212.png"
+  }
+]);
